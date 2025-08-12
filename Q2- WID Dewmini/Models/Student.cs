eba59b2bd712b5f5836addfeaaa98847a5760094
Dasuni363 <dasunidewmini363@gmail.com>
2025-08-12 23:42:38 +0530

    Model created

---
+++ Q2- WID Dewmini/Models/Student.cs
@@ -0,0 +1,17 @@
+﻿namespace Q2__WID_Dewmini.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+
+        public string StudentId { get; set; }
+
+        public string Name { get; set; }
+
+        public string City { get; set; }
+
+        public string CourseCode { get; set; }
+
+        
+    }
+}
