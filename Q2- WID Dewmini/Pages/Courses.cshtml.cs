3233ba4f7eb000d22e0767b7a2e7f2252dd5931d
Dasuni363 <dasunidewmini363@gmail.com>
2025-08-14 11:13:47 +0530

    Nav Bar Updated

---
+++ Q2- WID Dewmini/Pages/Courses.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Q2__WID_Dewmini.Pages
+{
+    public class CoursesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
